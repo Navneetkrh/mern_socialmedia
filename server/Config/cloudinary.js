a2d0bfd8801e020b035c97da0384f4a842a27e5b
Navneetkrh <108091719+Navneetkrh@users.noreply.github.com>
2023-10-11 21:11:46 +0530

    only jpg going

--- server/Config/cloudinary.js
+++ server/Config/cloudinary.js
@@ -1,10 +1,9 @@
 const cloudinary = require("cloudinary");
 const dotenv = require("dotenv");
-dotenv.config();
 
 
 cloudinary.config({
-  cloud_name:"dkqzl8erb",
+  cloud_name: "dkqzl8erb",
   api_key: "875217753844441",
   api_secret: "0WrLDpWLYhoFm4cqlfiCSgTVoPQ",
 });
